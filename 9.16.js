ee93d6884ab849155d8ee9d3ce63eb8c3fce828a
25LP1083 25LP1083 <25LP1083@pinecones-iMac.local>
2025-10-16 12:53:14 +0800

    first

--- 9.16.js
+++ 9.16.js
@@ -1,6 +1,6 @@
 let num = 0;
-document.addEventListener("click", () => {
-  let result = document.querySelector(".number");
+number.addEventListener("click", () => {
+  let result = number.querySelector(".number");
   result.innerHTML = num++;
 });
 // document.addEventListener("click", () => {
